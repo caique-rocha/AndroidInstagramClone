683b52a80ed463ec8793be98c1732c1166f38d51
Dragos Holban <dragos@intelligentbee.com>
2018-04-26 19:23:12 +0300

    sign in/sign out

--- app/src/main/java/com/dragosholban/instagram/MainActivity.java
+++ app/src/main/java/com/dragosholban/instagram/MainActivity.java
@@ -1,11 +1,19 @@
 package com.dragosholban.instagram;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -16,16 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
 
+    TextView textView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create and launch sign-in intent
-        startActivityForResult(
-                // Get an instance of AuthUI based on the default app
-                AuthUI.getInstance().createSignInIntentBuilder().build(),
-                RC_SIGN_IN);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        updateUI(user);
     }
 
     @Override
@@ -38,11 +49,43 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // ...
+                updateUI(user);
             } else {
                 // Sign in failed, check response for error code
-                // ...
+                Toast.makeText(this, response.getError().getMessage(), Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    public void buttonClick(View view)
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            // sign out
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+        } else {
+            // sign in
+            startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN);
+        }
+    }
+
+    private void updateUI(FirebaseUser user)
+    {
+        if (user != null) {
+            textView.setText("Hello " + user.getDisplayName() + "!");
+            button.setText("Sign Out");
+        } else {
+            textView.setText("Hello! Please sign in.");
+            button.setText("Sign In");
         }
     }
 }
