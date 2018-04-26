6af2b1890ec260f363107c38ce6f7bd3f200e579
Dragos Holban <dragos@intelligentbee.com>
2018-04-26 19:42:46 +0300

    load image from gallery

--- app/src/main/java/com/dragosholban/instagram/FeedActivity.java
+++ app/src/main/java/com/dragosholban/instagram/FeedActivity.java
@@ -1,13 +1,40 @@
 package com.dragosholban.instagram;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FeedActivity extends AppCompatActivity {
+
+    static final int RC_PERMISSION_READ_EXTERNAL_STORAGE = 3;
+    static final int RC_IMAGE_GALLERY = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    public void uploadImageClick(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, RC_PERMISSION_READ_EXTERNAL_STORAGE);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, RC_IMAGE_GALLERY);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+        }
     }
 }
